e712873ed917199d845812a3e17b51719fc59c5c
Bixzare <djibrilla.boubacar.502@gmail.com>
2025-05-26 11:08:48 +0100

    secrey key gen if not exist

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 import qrcode
 from placeholder import ids,names
 from cryptography.fernet import Fernet
+import os
+key_path = "secret.key"
 
-with open("secret.key") as key_file:
-    key = key_file.read()
-# key = Fernet.generate_key()
+# Check if the key already exists
+if not os.path.exists(key_path):
+    # Generate a new key and save it
+    key = Fernet.generate_key()
+    with open(key_path, "wb") as key_file:
+        key_file.write(key)
+else:
+    # Load the existing key
+    with open(key_path, "rb") as key_file:
+        key = key_file.read()
 
-# # Optionally save the key
-# with open("secret.key", "wb") as key_file:
-#     key_file.write(key)
-
-# # Step 2: Load the key (use this whenever encrypting/decrypting)
-# with open("secret.key", "rb") as key_file:
-#     key = key_file.read()
 
 f = Fernet(key)
 
@@ -35,21 +37,4 @@ def qr_gen(res,n):
 
 
 qr_gen(res,n)
-# lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-
-
-# message = lorem.encode()
-
-# token = f.encrypt(message)
-
-# img = qrcode.make(token)
-
-# img.save("qr_code.png")
-
-
-# print("Encrypted message:", token)
-
-# decrypted = f.decrypt(token).decode()
-
-# print("Decrypted message:", decrypted)
 
